9feb1e83300a5536b8d54887155edbfa062c223d
Moritz Inderwies <moritz.inderwies@mercedes-benz.com>
2025-06-22 16:23:08 +0200

    main

--- Tree_Tools.cpp
+++ Tree_Tools.cpp
@@ -1,1 +1,157 @@
+#include <iostream>
 
+struct LinkedTree {
+    char value;
+    LinkedTree *left;
+    LinkedTree *right;
+
+    LinkedTree(char val) : value(val), left(nullptr), right(nullptr) {
+    }
+
+    LinkedTree(LinkedTree *left, char val, LinkedTree *right): value(val), left(left), right(right) {
+    };
+};
+
+class TreeTools {
+public:
+    /**
+    * Ermittelt die Hoehe eines Baumes
+    * @param b der zu uebergebende Baum
+    * @return Hoehe des Baumes
+    */
+    static int treeHeight(LinkedTree *b) {
+        if (b == nullptr) return 0;
+        return 1 + std::max(treeHeight(b->left), treeHeight(b->right));
+        //Rekursive Addition der Höhe des Linken oder Rechten Baumteiles
+    }
+
+    /**
+    * Ermittelt die Anzahl der Knoten eines Baumes
+    * @param b der zu uebergebende Baum
+    * @return Anzahl der Knoten des Baumes
+    */
+    static int anzahlKnoten(LinkedTree *b) {
+        if (b == nullptr) return 0;
+        return 1 + anzahlKnoten(b->left) + anzahlKnoten(b->right);
+        //Rekursive Addition der anzahl der Knoten des Linken und Rechten Baumteiles
+    }
+
+    static void printTreeInorder(LinkedTree *b) {
+        if (b == nullptr) return;
+        printTreeInorder(b->left);
+        std::cout << b->value;
+        printTreeInorder(b->right);
+    }
+
+    /**
+    * gibt den übergebenen Baum in Inorder-Traversierung mit Klammerung aus
+    * @param b der zu uebergebende Baum
+    */
+
+    static void printTreeInorderWithParenthesis(LinkedTree *b) {
+        // TODO
+    }
+
+
+    /**
+    * gibt den übergebenen Baum in Levelorder-Traversierung aus
+    * @param b der zu uebergebende Baum
+    */
+    static void printTreeLevelorder(LinkedTree *b) {
+        // TODO
+    }
+
+    /**
+    * Sortiert gegebene Zahlenfolge per Suchbaum
+    * @param zahlen zu sortierende Zahlenfolge
+    * @return sortierte Folge
+    */
+    static int *searchTreeSort(int *zahlen) {
+        // TODO
+    }
+
+    /**
+    * Helfermethode zum sortieren von Elementen im SearchTree
+    * @param b SearchTree mit zu sortierendem Inhalt
+    * @param k Stack in den der SearchTree Inhalt sortiert werden soll
+    */
+    /*
+    static void tree2SortedStack(LinkedTree* b, Stack* k) {
+        // TODO
+
+    }
+    */
+
+    /**
+    * Druckt einen Baum auf der Konsole ebenenweise aus.
+    * Nutzt treeHeight(Tree), printLevel(Tree,int,int) und spaces(int).
+    * @param b der zu druckende Baum
+    */
+    static void printTree(LinkedTree *b) {
+        // Berechne die Hoehe des Baumes
+        int resthoehe = treeHeight(b);
+
+        //Gehe die Ebenen des Baumes durch
+        for (int i = 0; i < resthoehe; i++) {
+            //Drucke die Ebene, beruecksichtige Anzahl der Leerzeichen
+            //fuer korrekte Einrueckung
+            printLevel(b, i, spaces(resthoehe - i));
+            std::cout << std::endl;
+            std::cout << std::endl;
+        }
+    }
+
+    /**
+    * Druckt eine Ebene eines Baumes aehnlich der Breitensuche.
+    * 0 ist dabei die Wurzel. Vor und nach jedem Element werden Leerzeichen
+    * eingefuegt.
+    * @param b der auszugebende Baum
+    * @param ebene die gewuenschte Ebene beginnend bei 0
+    * @param spaces Anzahl von Leerzeichen vor und nach jedem Element
+    */
+    static void printLevel(LinkedTree *b, int level, int spaces) {
+        // Wenn 0 erreicht, drucke Element mit Leerzeichen
+        if (level == 0) {
+            for (int i = 0; i < spaces; i++) std::cout << " ";
+            if (b != nullptr) {
+                std::cout << b->value;
+            } else {
+                // Wenn Nullzeiger uebergeben wurde, ein Leerzeichen drucken
+                std::cout << " ";
+            }
+            for (int i = 0; i <= spaces; i++)
+                std::cout << " ";
+        } else {
+            // Steige rekursiv ab, betrachte Soehne als Teilbaeume und verringere
+            // dabei die zu druckende Ebene. In endende Aeste wird mit dem Nullzeiger
+            // gelaufen.
+            if (b != nullptr && b->left != nullptr) {
+                printLevel(b->left, level - 1, spaces);
+            } else {
+                printLevel(nullptr, level - 1, spaces);
+            }
+
+            if (b != nullptr && b->right != nullptr) {
+                printLevel(b->right, level - 1, spaces);
+            } else {
+                printLevel(nullptr, level - 1, spaces);
+            }
+        }
+    }
+
+    /**
+    * Berechnet die Anzahl an benoetigten Leerzeichen fuer
+    * eine korrekte Einrueckung, also 0.5 * (2^(level) - 2).
+    * @param level die Ebene, Blaetter sind Ebene 1, darueber aufsteigend
+    * @return Anzahl der Leerzeichen zur Elementtrennung
+    */
+    static int spaces(int level) {
+        if (level == 1) {
+            return 0;
+        } else {
+            // verdoppele die Leerzeichen gegenueber der Ebene darunter
+            // und fuege ein weiteres Leerzeichen hinzu
+            return 2 * spaces(level - 1) + 1;
+        }
+    }
+};
---
+++ Tree_Tools_Test.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "Tree_Tools.cpp"
+
+int main() {
+
+    // Erzeuge einen Test-Baum
+    LinkedTree* a = new LinkedTree('A');
+    LinkedTree* b = new LinkedTree('B');
+    LinkedTree* m = new LinkedTree(a, '*', b);
+    LinkedTree* f = new LinkedTree('F');
+    LinkedTree* p = new LinkedTree(f, '+', m);
+    LinkedTree* x = new LinkedTree('X');
+    LinkedTree* y = new LinkedTree('Y');
+    LinkedTree* n = new LinkedTree(x, '-', y);
+    LinkedTree* d = new LinkedTree(p, '/', n);
+
+
+    // Testet treeHeight(), anzahlKnoten(),
+    // printTreeInorderWithParenthesis(), printTreeLevelorder()
+
+        TreeTools::printTree(d);
+    //
+       std::cout << "Baumhoehe laut Methode (sollte 4 sein): "
+                << TreeTools::treeHeight(d) << std::endl;
+    //
+    std::cout << "Anzahl Knoten laut Methode (sollte 9 sein): "
+                  << TreeTools::anzahlKnoten(d) << std::endl;
+    TreeTools::printTreeInorder(d);
+    //
+    //    std::cout << "Ausdruck mit Klammerung: ";
+    //    TreeTools::printTreeInorderWithParenthesis(d);
+    //    std::cout << std::endl;
+    //
+    //    std::cout << "Baum ebenenweise: ";
+    //    TreeTools::printTreeLevelorder(d);
+    //    std::cout << std::endl;
+
+
+    // Testet searchTreeSort()
+
+    //    std::cout << "Sortiere  6194283" << std::endl;
+    //    int a[] = {6,1,9,4,2,8,3};
+    //    int* sorted = TreeTools::searchTreeSort(a);
+    //    std::cout << "Ergebnis (sollte 1234689 sein): ";
+    //    for(int i = 0; i < sizeof(a)/sizeof(int); i++)
+    //      std::cout << sorted[i];
+
+    std::cout << std::endl;
+
+    return 0;
+}
